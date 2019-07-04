db913eb3da4f7923a9ba91bbd89baa00316bf90a
gokhanald <gokhanaldanmaz01@gmail.com>
2019-07-04 17:04:59 +0300

    Klasör Yapısı Oluştu

--- Deneme1/Controllers/HomeController.cs
+++ Deneme1/Controllers/HomeController.cs
@@ -4,7 +4,7 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using Deneme1.Models;
+
 
 namespace Deneme1.Controllers
 {
@@ -20,10 +20,6 @@ namespace Deneme1.Controllers
             return View();
         }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
+       
     }
 }
